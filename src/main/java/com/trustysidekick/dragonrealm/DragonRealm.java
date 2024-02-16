c5a2a7577442e62b1370be12b8126f551ba1c0d3
Garold Merritt <merrittjerry@gmail.com>
2024-02-16 11:05:52 -0500

    Testing colaborated Commits
    Signed-off-by: Garold Merritt <merrittjerry@gmail.com>

--- src/main/java/com/trustysidekick/dragonrealm/DragonRealm.java
+++ src/main/java/com/trustysidekick/dragonrealm/DragonRealm.java
@@ -1,22 +1,25 @@
 package com.trustysidekick.dragonrealm;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DragonRealm implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final String MOD_ID = "dragonrealm";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+
+		Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
 
 		LOGGER.info("Hello Fabric world!");
 	}
